f21ca7f9fb26410bbbc6ed9d88131c24cdf875e3
ymdzq <233900743@qq.com>
2012-11-23 14:30:46 +0800

    fix wifi

--- releasetools.py
+++ releasetools.py
@@ -1,28 +1,14 @@
 import common
 import edify_generator
 
-def DeleteFormat(info):
+def ReplaceLine(info):
     edify = info.script
     for i in xrange(len(edify.script)):
-        if "format" in edify.script[i] and "/dev/block/mmcblk0p25" in edify.script[i]:
-            edify.script[i]="""format("ext4", "EMMC", "/dev/block/mmcblk0p25", "0");"""
-        elif "mount" in edify.script[i] and "/dev/block/mmcblk0p25" in edify.script[i]:
-            edify.script[i]="""mount("ext4", "EMMC", "/dev/block/mmcblk0p25", "/system");"""
-    return
-
-def AddAssertions(info):
-    info.script.AppendExtra('set_perm_recursive(0, 2000, 06755, 06755, "/system/xbin");');
-    info.script.AppendExtra('set_perm_recursive(0, 2000, 0755, 0750, "/system/etc/init.d");');
-    info.script.AppendExtra('set_perm(0, 0, 0755, "/system/etc/init.d");');
-    return
+        if 'set_perm_recursive(0, 2000, 0777, 0755, "/system/xbin");' in edify.script[i]:
+            edify.script[i] = 'set_perm_recursive(0, 2000, 0777, 0755, "/system/xbin");\nset_perm_recursive(0, 2000, 0755, 0750, "/system/etc/init.d");\nset_perm(0, 0, 0755, "/system/etc/init.d");'
 
 def FullOTA_InstallEnd(info):
-    DeleteFormat(info)
-    AddAssertions(info)
+    ReplaceLine(info)
 
 def IncrementalOTA_InstallEnd(info):
-    edify = info.script
-    for i in xrange(len(edify.script)):
-        if "mount" in edify.script[i] and "/dev/block/mmcblk0p25" in edify.script[i]:
-            edify.script[i]="""mount("ext4", "EMMC", "/dev/block/mmcblk0p25", "/system");"""
-    AddAssertions(info)
+    ReplaceLine(info)
