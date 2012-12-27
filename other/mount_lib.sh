2b02386e535e19ae2e03f2a96f1ef4be53e09776
ymdzq <233900743@qq.com>
2012-12-27 18:07:26 +0800

    update 2.12.27: Add mechanism of auto-start management

--- other/mount_lib.sh
+++ other/mount_lib.sh
@@ -4,19 +4,24 @@ mkdir -p /system
 mkdir -p /system/lib
 mount /dev/block/mmcblk0p25 /system
 mount /dev/block/mmcblk0p28 /system/lib
-SIZE=`df | busybox grep "/system/lib" | busybox awk '{print $1}'`
-if [ $SIZE > 102400 ]; then
-    echo "the partition is mmcblk0p28 lib, return"
-    exit 0 
-else
-    mount /dev/block/mmcblk0p29 /system/lib
+if [ $? -eq 0 ]; then
     SIZE=`df | busybox grep "/system/lib" | busybox awk '{print $1}'`
     if [ $SIZE > 102400 ]; then
+        echo "the partition is mmcblk0p28 lib, return"
+        exit 0
+    fi
+fi
+
+mount /dev/block/mmcblk0p29 /system/lib
+if [ $? -eq 0 ]; then
+SIZE=`df | busybox grep "/system/lib" | busybox awk '{print $1}'`
+    if [ $SIZE > 102400 ]; then
         echo "the partition mmcblk0p29 is lib, return"
-        exit 0 
-    else 
+        exit 0
+    else
         echo "not find partition"
         exit -1
     fi
 fi
+echo "not find partition"
 
